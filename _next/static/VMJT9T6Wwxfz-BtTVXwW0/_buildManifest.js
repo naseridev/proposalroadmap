5d14bab0c242b1c3e7453e4acd498d31dd39b306
Nima Naseri <naseridev@gmail.com>
2026-01-04 09:18:57 -0800

    Updates

--- _next/static/VMJT9T6Wwxfz-BtTVXwW0/_buildManifest.js
+++ _next/static/VMJT9T6Wwxfz-BtTVXwW0/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/proposalroadmap//_next/:path+",
+        "destination": "/proposalroadmap/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
